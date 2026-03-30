4b15ebefdc533aa61021ccb184c919a118e8a052
NxterM <ccarvajal@lyntec.cl>
2026-03-30 08:16:00 -0300

    03.30.01

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const CL_TIMEZONE = "America/Santiago";
-const APP_VERSION = "Totem v2026.03.27.09";
+const APP_VERSION = "Totem v2026.03.30.01";
 
 const input = document.getElementById("rutInput");
 const btn = document.getElementById("btnConfirmar");
